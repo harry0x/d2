6edf90c8e4ad737924278cdf0d38db244206e9fb
harry0x <hari.websyst@gmail.com>
2026-02-02 16:30:10 +0530

    Refactor server.js to implement note management endpoints

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.get("/post", (req, res) => {
-  res.send("hey");
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  console.log(notes);
+  res.send("created");
+});
+
+app.get("/getnotes", (req, res) => {
+  res.send(notes);
+});
+app.delete("/deletenotes/:index", (req, res) => {
+  delete notes[req.params.index];
+  res.send("deleted");
+});
+
+app.patch("/updatenotes/:index", (req, res) => {
+  notes[req.params.index].des = req.body.des;
+  res.send("updated");
 });
 
 app.listen(3000);
