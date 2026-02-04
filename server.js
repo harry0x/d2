d31222a9e77a94128a8456090bb83fa7dbef4540
harry0x <hari.websyst@gmail.com>
2026-02-04 15:20:02 +0530

    Refactor response handling for note management endpoints

--- server.js
+++ server.js
@@ -10,23 +10,33 @@ app.get("/", (req, res) => {
 const notes = [];
 
 app.post("/notes", (req, res) => {
-  console.log(req.body);
   notes.push(req.body);
-  console.log(notes);
-  res.send("created");
+  res.status(201).json({
+    message: "Note Created",
+  });
 });
 
 app.get("/getnotes", (req, res) => {
-  res.send(notes);
+  // res.send(notes);
+  res.status(200).json({
+    notes: notes,
+  });
 });
 app.delete("/deletenotes/:index", (req, res) => {
   delete notes[req.params.index];
-  res.send("deleted");
+  // res.send("deleted");
+  // message not showing
+  res.status(204).json({
+    message: "Note Deleted ",
+  });
 });
 
 app.patch("/updatenotes/:index", (req, res) => {
   notes[req.params.index].des = req.body.des;
-  res.send("updated");
+  // res.send("updated");
+  res.status(200).json({
+    message: "updated",
+  });
 });
 
 app.listen(3000);
