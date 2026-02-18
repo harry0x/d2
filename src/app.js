1194a63c29a716d224c02035e8e6a47d3abc4e88
harry0x <hari.websyst@gmail.com>
2026-02-18 21:17:14 +0530

    Add initial HTML structure for the frontend application

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@
 
 const express = require("express");
 const noteModel = require("./models/notes.model");
-const app = express();
 const cors = require("cors");
+const path = require("path");
 
-app.use(express.json());
+const app = express();
 app.use(cors());
-
+app.use(express.json());
+app.use(express.static("./public"));
 
 ///create notes
 app.post("/notes", async (req, res) => {
@@ -21,34 +22,38 @@ app.post("/notes", async (req, res) => {
 });
 
 // get notes
-app.get('/notes', async(req,res) => {
-   const notes = await noteModel.find()
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find();
 
-   res.status(200).json({
+  res.status(200).json({
     message: "Noted fetched Successfully",
-    notes
-   })
-})
+    notes,
+  });
+});
 
-// delete nots 
+// delete nots
 
-app.delete('/notes/:id' , async (req,res) => {
-    const id = req.params.id
-    await noteModel.findByIdAndDelete(id)
-    res.status(200).json({
-        message:"Note deleted Successfully",
-    })
-})
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  await noteModel.findByIdAndDelete(id);
+  res.status(200).json({
+    message: "Note deleted Successfully",
+  });
+});
 
-// update notes 
-app.patch('/notes/:id', async (req,res) => {
-    const id = req.params.id
-    const{description} = req.body
-    await noteModel.findByIdAndUpdate(id, {description})
+// update notes
+app.patch("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { description } = req.body;
+  await noteModel.findByIdAndUpdate(id, { description });
 
-    res.status(200).json({
-        message: "Note updated Successfully"
-    })
-})
+  res.status(200).json({
+    message: "Note updated Successfully",
+  });
+});
+
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
 
 module.exports = app;
